a9467f65cb120ab5f37880248ada2673d74e2432
João M. Bezerra <marcospb19@hotmail.com>
2022-02-28 20:24:50 -0300

    Refactor whole code

--- src/bookkeeper.rs
+++ src/bookkeeper.rs
@@ -1,39 +1,110 @@
 use std::{
-    io::{Read, Seek},
-    path::Path,
+    io::{Read, Seek, Write},
+    path::PathBuf,
 };
 
-use crate::error::{Error, Result, TomlTypeCheck, TomlTypeCheckDiagnosis};
-
+use bigdecimal::{BigDecimal, Zero};
 use fs_err as fs;
 use toml::value::{Table as TomlTable, Value as TomlValue};
 
+use crate::{
+    error::{Error, Result, TomlTypeCheck, TomlTypeCheckDiagnosis},
+    parser::{Entry, EntryType},
+};
+
+#[derive(Debug)]
+pub struct BookkeeperStatus {
+    /// Total amount spended
+    pub outgoing_total: BigDecimal,
+    /// Total amount received
+    pub incoming_total: BigDecimal,
+    // /// Each spend transaction
+    // pub outgoing: Vec<BigDecimal>,
+    // /// Each receive transaction
+    // pub incoming: Vec<BigDecimal>,
+}
+
+impl BookkeeperStatus {
+    pub fn display(&self) {
+        println!("\tIncoming: R$ {}", self.incoming_total);
+        println!("\tOutgoing: R$ {}", self.outgoing_total);
+    }
+}
+
 pub struct Bookkeeper {
     pub file: fs::File,
+    pub file_path: PathBuf,
     pub file_contents: String,
     pub table: TomlTable,
+    pub status: BookkeeperStatus,
 }
 
 impl Bookkeeper {
+    pub fn display_status(&self) {
+        // Safety: Always has file name because it's in format "MM-YYYY"
+        println!("Status for {:?}", self.file_path.file_name().unwrap());
+
+        self.status.display();
+    }
+
     // Returns opened file and it's contents
     //
     // Seeked to the start, writes will overwrite from the start
-    pub fn load_from_path(path: &Path) -> Result<Self> {
-        let mut file = fs::OpenOptions::new().read(true).write(true).open(path)?;
+    pub fn load_from_path(path: impl Into<PathBuf>) -> Result<Self> {
+        let path = path.into();
+        let mut file = fs::OpenOptions::new().read(true).write(true).open(&path)?;
         let mut file_contents = String::new();
         file.read_to_string(&mut file_contents)?;
         file.rewind()?;
 
-        let (table, type_check_diagnosis) = Self::load_toml_table_or_default(&file_contents);
+        let table = Self::load_toml_table_or_default(&file_contents);
 
+        let type_check_diagnosis = type_check_toml_fields(&table);
         if type_check_diagnosis.has_error_description() {
-            Err(Error::InvalidTomlTypes {
+            return Err(Error::InvalidTomlTypes {
                 description: type_check_diagnosis.into_inner(),
-                path: path.into(),
-            })
-        } else {
-            Ok(Self { file, file_contents, table })
+                path,
+            });
         }
+
+        let status = Self::status_from_toml_table(&table)?;
+
+        Ok(Self {
+            file,
+            file_path: path,
+            file_contents,
+            table,
+            status,
+        })
+    }
+
+    pub fn add_entry(&mut self, entry: Entry) -> Result<()> {
+        let (array_key, kind_symbol) = match entry.kind {
+            EntryType::Withdraw => ("take", '-'),
+            EntryType::Deposit => ("put", '+'),
+        };
+
+        let line = format!(
+            "{d} {k} {a} {D}",
+            d = entry.day,
+            k = kind_symbol,
+            a = entry.amount,
+            D = entry.description
+        );
+
+        self.table[array_key]
+            .as_array_mut()
+            .unwrap()
+            .push(line.into());
+
+        let temporary_toml = TomlValue::Table(std::mem::take(&mut self.table));
+        let toml = toml::ser::to_string_pretty::<TomlValue>(&temporary_toml).unwrap();
+        self.table = unwrap_toml_table(temporary_toml);
+        write!(self.file, "{}", toml)?;
+        truncate_and_close_file(&mut self.file)?;
+        println!("Updated {}", self.file_path.display());
+
+        Ok(())
     }
 
     /// Loads toml table from text and type check, or generate default
@@ -41,23 +112,37 @@ impl Bookkeeper {
     /// # Errors:
     ///
     /// Returns `Error::InvalidTomlTypes` if loaded toml has incorrect types.
-    fn load_toml_table_or_default(input_text: &str) -> (TomlTable, TomlTypeCheckDiagnosis) {
+    fn load_toml_table_or_default(input_text: &str) -> TomlTable {
         let toml = if input_text.trim().is_empty() {
             generate_default_toml()
         } else {
             input_text.parse().unwrap()
         };
 
-        let unwrap_toml_table = |toml: TomlValue| -> TomlTable {
-            match toml {
-                TomlValue::Table(table) => table,
-                _ => unreachable!(),
-            }
-        };
+        unwrap_toml_table(toml)
+    }
 
-        let table = unwrap_toml_table(toml);
-        let type_check_diagnosis = type_check_toml_fields(&table);
-        (table, type_check_diagnosis)
+    /// Read a bookkeeping file and return the total amount spent and received.
+    pub fn status_from_toml_table(table: &TomlTable) -> Result<BookkeeperStatus> {
+        let mut outgoing_total = BigDecimal::zero();
+        let mut incoming_total = BigDecimal::zero();
+
+        let (take, put) = (
+            table["take"].as_array().unwrap(),
+            table["put"].as_array().unwrap(),
+        );
+
+        for entry in take.iter().chain(put) {
+            let entry = entry.as_str().unwrap();
+            let entry = Entry::from_str(entry).unwrap();
+
+            match entry.kind {
+                EntryType::Withdraw => outgoing_total += entry.amount,
+                EntryType::Deposit => incoming_total += entry.amount,
+            }
+        }
+
+        Ok(BookkeeperStatus { outgoing_total, incoming_total })
     }
 }
 
@@ -102,4 +187,57 @@ pub fn generate_default_toml() -> TomlValue {
     // map.insert("put".to_string(), TomlValue::Array(vec![]));
 
     toml
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{io::Write, str::FromStr};
+
+    use bigdecimal::BigDecimal;
+    use tempfile::NamedTempFile;
+
+    use super::*;
+
+    #[test]
+    fn reads_income_and_outcome_total_from_file_correctly() {
+        let mut dummy = NamedTempFile::new().unwrap();
+
+        let toml = toml::toml! {
+            put = [
+                "22 + 200.50 Payment",
+                "22 + 300.25 Another Payment",
+            ]
+            take = [
+                "23 - 10.25 Lunch",
+                "23 - 10.27 Dinner",
+                "24 - 400.00 kindle-para-bish",
+            ]
+        };
+        writeln!(dummy, "{}", toml).unwrap();
+
+        let bookkeeper = Bookkeeper::load_from_path(dummy.path()).unwrap();
+        let status = bookkeeper.status;
+
+        assert_eq!(
+            status.incoming_total,
+            BigDecimal::from_str("500.75").unwrap()
+        );
+        assert_eq!(
+            status.outgoing_total,
+            BigDecimal::from_str("420.52").unwrap()
+        );
+    }
+}
+
+// Truncates file to have only the written content
+fn truncate_and_close_file(file: &mut fs::File) -> Result<()> {
+    let written_len = file.stream_position()?;
+    file.set_len(written_len).map_err(Into::into)
+}
+
+fn unwrap_toml_table(toml: TomlValue) -> TomlTable {
+    match toml {
+        TomlValue::Table(table) => table,
+        _ => unreachable!(),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,7 @@ mod dirs;
 mod error;
 mod file;
 mod parser;
-mod reader;
-mod writer;
 
-use std::path::PathBuf;
-
-use bigdecimal::BigDecimal;
 use chrono::{Datelike, Local};
 use clap::Parser;
 use dirs::Dirs;
@@ -24,16 +19,7 @@ use crate::{
     cli::{Opts, Subcommand},
     file::create_file_if_not_existent,
     file::BookkeepingFile,
-    writer::Writer,
 };
-
-#[derive(Debug)]
-pub struct Status {
-    /// Amount spended
-    pub outgoing: BigDecimal,
-    /// Amount received
-    pub incoming: BigDecimal,
-}
 
 fn main() {
     if let Err(err) = exec() {
@@ -42,53 +28,45 @@ fn main() {
     }
 }
 
+fn exec() -> Result<()> {
+    GlobalState::new()?.run_command()
+}
+
 struct GlobalState {
-    opts: Opts,
-    // Bookkeeping path
-    bk_path: PathBuf,
+    cmd: Subcommand,
+    bookkeeper: Bookkeeper,
 }
 
 impl GlobalState {
     pub fn new() -> Result<Self> {
-        let opts = Opts::parse();
+        let cmd = Opts::parse().cmd;
         let dirs = Dirs::init()?;
 
         let bk_path = dirs.data().join(BookkeepingFile::current_file().as_path());
         create_file_if_not_existent(&bk_path);
+        let bookkeeper = Bookkeeper::load_from_path(bk_path)?;
 
-        Ok(Self { opts, bk_path })
+        Ok(Self { cmd, bookkeeper })
     }
 
     pub fn run_command(self) -> Result<()> {
         let day = Local::today().day() as u8;
-        let Self {
-            ref bk_path,
-            opts: Opts { cmd },
-            ..
-        } = self;
+        let Self { cmd, mut bookkeeper } = self;
 
         match cmd {
             Subcommand::Take { amount, ref description } => {
                 let entry = Entry::new(day, EntryType::Withdraw, amount, description);
-                Writer::write_entry(bk_path, entry)?;
+                bookkeeper.add_entry(entry)?;
             }
             Subcommand::Put { amount, ref description } => {
                 let entry = Entry::new(day, EntryType::Deposit, amount, description);
-                Writer::write_entry(bk_path, entry)?;
+                bookkeeper.add_entry(entry)?;
             }
             Subcommand::Status => {
-                let total = reader::status_from_file(bk_path)?;
-                // Safeyu: Always has file name because it's in format "MM-YYYY"
-                println!("Status for {:?}", bk_path.file_name().unwrap());
-                println!("\tIncoming: R$ {}", total.incoming);
-                println!("\tOutgoing: R$ {}", total.outgoing);
+                bookkeeper.display_status();
             }
         };
 
         Ok(())
     }
-}
-
-fn exec() -> Result<()> {
-    GlobalState::new()?.run_command()
 }
